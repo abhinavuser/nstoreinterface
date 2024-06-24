41b55c0c3f408d91a3aa0a59fdcb95e6718c59dc
Abhinav-2101 <abhinavkumarv2101@gmail.com>
2024-06-24 16:05:32 +0530

    navbar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,95 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <nav>
+      <ul>
+        <li v-for="(option, index) in navbarOptions" :key="index" class="nav-item">
+          <a v-if="typeof option === 'string'" @click="handleOptionClick(option)" class="nav-link">{{ option }}</a>
+          <div v-else class="dropdown">
+            <button @click="toggleDropdown(option)" class="dropbtn">{{ option.title }}</button>
+            <div v-show="option.open" class="dropdown-content">
+              <a v-for="(subOption, subIndex) in option.subOptions" :key="subIndex" @click="handleOptionClick(subOption)" class="nav-link">{{ subOption }}</a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <div v-if="selectedOption">
+      <h1>{{ selectedOption }} Page</h1>
+      <!-- Your order page content here -->
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      navbarOptions: [
+        "Home",
+        "About",
+        { title: "Services", open: false, subOptions: ["Service 1", "Service 2", "Service 3"] },
+        { title: "Order", open: false, subOptions: ["Order1", "Order2"] }
+      ],
+      selectedOption: null
+    };
+  },
+  methods: {
+    handleOptionClick(option) {
+      this.selectedOption = option;
+    },
+    toggleDropdown(option) {
+      option.open = !option.open;
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.nav-item {
   display: inline-block;
-  margin: 0 10px;
+  margin-right: 10px;
 }
-a {
-  color: #42b983;
+
+.nav-link {
+  color: #000;
+  text-decoration: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropbtn {
+  background-color: #3498db;
+  color: white;
+  padding: 10px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  display: block;
+  padding: 12px 16px;
+  color: black;
+  text-decoration: none;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
 }
 </style>
