9af9ba89ca566c95759c10c001aa5b2e296f8c8a
PrajanS <prajan200612@gmail.com>
2024-07-05 12:09:04 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
